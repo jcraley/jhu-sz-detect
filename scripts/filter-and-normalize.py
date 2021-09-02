653dcb2ccdf78c3ac73c838e0dcb2d8c0be241f7
Jeff Craley <jcraley@gmail.com>
2021-09-02 18:42:21 -0400

    correcting path issues and creating example script

--- scripts/filter-and-normalize.py
+++ scripts/filter-and-normalize.py
@@ -1,6 +1,8 @@
 import os
 import sys
-sys.path.append("..")
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 from preprocessing.eeg_info import EegInfo
 from preprocessing.edf_loader import EdfLoader
 import utils.testconfiguration as tc
@@ -8,6 +10,7 @@ import utils.read_files as read
 import utils.pathmanager as pm
 import preprocessing.dsp as dsp
 import torch
+
 
 def main():
     """Load the command line args and parse"""
--- scripts/standardize-features.py
+++ scripts/standardize-features.py
@@ -1,8 +1,10 @@
 import os
 import sys
-
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+print(sys.path)
 import torch
-
 import preprocessing.features as features
 import utils.pathmanager as pm
 import utils.read_files as read
